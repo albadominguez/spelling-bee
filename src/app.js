2ef1c5931857d7cf95ab4a3a0934348a097c998a
Alba Dominguez <adominguezpera@gmail.com>
2022-03-17 18:15:19 +0100

    Random letters
    random letters function

---
+++ src/app.js
@@ -0,0 +1,29 @@
+function chooseLetter() {
+  let text = ""
+  let possible = Array.from("abcdefghijklmnopqrstuvwxyz")
+
+  let vowels = Array.from("aeiou")
+
+  for (let i = 0; i < 6; i++) {
+    let letterPositon = Math.floor(Math.random() * possible.length)
+    text += possible[letterPositon]
+    possible.splice(letterPositon, 1)
+  }
+
+  if (
+    text.includes("a") ||
+    text.includes("e") ||
+    text.includes("i") ||
+    text.includes("o") ||
+    text.includes("u")
+  ) {
+    let letterPositon = Math.floor(Math.random() * possible.length)
+    text += possible[letterPositon]
+  } else {
+    text += vowels[Math.floor(Math.random() * vowels.length)]
+  }
+
+  return Array.from(text)
+}
+
+console.log(chooseLetter())
